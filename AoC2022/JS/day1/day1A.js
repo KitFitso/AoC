0478731f4937abf4dd5b33597815172411ee22a4
ky peck <kypeckmail@gmail.com>
2023-05-01 20:51:48 -0700

    push

--- AoC2022/JS/day1/day1A.js
+++ AoC2022/JS/day1/day1A.js
@@ -1,12 +1,12 @@
-const input = require("../inputs/day1");
+const input = require('../inputs/day1');
 
-const calories = input.split("\n\n");
+const calories = input.split('\n\n');
 let max = 0;
 
 for (let i in calories) {
-  calories[i] = calories[i].split("\n");
+  calories[i] = calories[i].split('\n');
   const newVal = calories[i].reduce((total, curr) => total + +curr, 0);
   if (newVal > max) max = newVal;
 }
 
-console.log(max);
+console.log(crypto.randomUUID());
--- AoC2022/JS/day1/day1B.js
+++ AoC2022/JS/day1/day1B.js
@@ -1,13 +1,20 @@
-const input = require("../inputs/day1");
+const input = require('../inputs/day1');
 
-const calories = input.split("\n\n");
+const calories = input.split('\n\n');
 
 for (let i in calories) {
   calories[i] = calories[i]
-    .split("\n")
+    .split('\n')
     .reduce((total, curr) => total + +curr, 0);
 }
 
 calories.sort((a, b) => a - b);
 
 console.log(calories.at(-1) + calories.at(-2) + calories.at(-3));
+
+response = openai.Image.create(
+  (prompt = 'a white siamese cat'),
+  (n = 1),
+  (size = '1024x1024')
+);
+image_url = response['data'][0]['url'];
